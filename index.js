d3d81794a0e884ae5f49d80841599ded64619150
jxfff <280796037@qq.com>
2022-01-11 11:28:36 +0800

    #fix

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const nodemailer = require("nodemailer");
 // const a = require("./ip.json");
 const fs = require("fs");
 
-let { ip } = JSON.parse(fs.readFileSync("./ip.json", "utf-8"));
-// import a from "./ip.json";
-console.log(
-  "%c [ ip ]: ",
-  "color: #bf2c9f; background: pink; font-size: 13px;",
-  ip
-);
+
 function recordIp(newIp, oldIp) {
   if (newIp !== oldIp) {
     console.log("diff");
@@ -22,14 +16,27 @@ function recordIp(newIp, oldIp) {
     fs.writeFileSync("./ip.json", JSON.stringify({ ip: newIp || "127.0.0.1" }));
   }
 }
-let newIp;
-http("http://ip-api.com/json")
-  .then((res) => {
-    console.log(res);
-    newIp = res.query;
-    recordIp(newIp, ip);
-    sendEmail(newIp);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+try{
+  let { ip } = JSON.parse(fs.readFileSync("./ip.json", "utf-8"));
+  // import a from "./ip.json";
+  console.log(
+    "%c [ ip ]: ",
+    "color: #bf2c9f; background: pink; font-size: 13px;",
+    ip
+  );
+  
+  let newIp;
+  http("http://ip-api.com/json")
+    .then((res) => {
+      console.log(res);
+      newIp = res.query;
+      recordIp(newIp, ip);
+      sendEmail(newIp);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}catch(e){
+  console.log(e);
+}
+
