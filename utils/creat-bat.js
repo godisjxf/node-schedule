f1b95785b798c6799f53ed868a532da473ec5dee
贾晓帆 <8476709+eatchuanchuan@user.noreply.gitee.com>
2022-01-11 12:56:17 +0800

    #change dir

--- utils/creat-bat.js
+++ utils/creat-bat.js
@@ -3,4 +3,14 @@ const path = require("path");
 let absolutePath = path.resolve(__dirname, "../");
 console.log(absolutePath);
 let file = `${absolutePath.slice(0, 2)}\r\ncd ${absolutePath}\r\npnpm test`;
+let email = {
+  user: "your email account",
+  pass: "your pass",
+};
+let ip = { ip: "" };
 fs.writeFileSync(path.resolve(absolutePath, "./run-bat.bat"), file);
+fs.writeFileSync(
+  path.resolve(absolutePath, "./email.json"),
+  JSON.stringify(email)
+);
+fs.writeFileSync(path.resolve(absolutePath, "./ip.json"), JSON.stringify(ip));
