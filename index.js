34024045450f7ab3c6dd1c374b2f0c80f41b17b3
贾晓帆 <8476709+eatchuanchuan@user.noreply.gitee.com>
2022-01-11 14:40:32 +0800

    # add log

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import http from "./utils/httpGet";
 import sendEmail from "./utils/email";
 // const a = require("./ip.json");
+var log4js = require("log4js");
 const fs = require("fs");
 
+//log4js 设置
+log4js.configure({
+  appenders: {
+    fileout: { type: "file", filename: "./log/fileout.log" },
+    datafileout: {
+      type: "dateFile",
+      filename: "./log/datafileout.log",
+      pattern: ".yyyy-MM-dd",
+      daysToKeep: 7,
+    },
+    consoleout: { type: "console" },
+  },
+  categories: {
+    default: { appenders: ["fileout", "consoleout"], level: "debug" },
+    ip: { appenders: ["datafileout"], level: "debug" },
+    anything: { appenders: ["consoleout"], level: "debug" },
+  },
+});
+let logger = log4js.getLogger("ip");
 
 function recordIp(newIp, oldIp) {
   if (newIp !== oldIp) {
@@ -12,18 +32,24 @@ function recordIp(newIp, oldIp) {
       "color: #bf2c9f; background: pink; font-size: 13px;",
       newIp
     );
-    sendEmail(newIp,()=>{ fs.writeFileSync("./ip.json", JSON.stringify({ ip: newIp || "127.0.0.1" }));});
+    sendEmail(newIp, () => {
+      fs.writeFileSync(
+        "./ip.json",
+        JSON.stringify({ ip: newIp || "127.0.0.1" })
+      );
+      logger.info(`now ip->${newIp}`);
+    });
   }
 }
-try{
-  let { ip:oldIp } = JSON.parse(fs.readFileSync("./ip.json", "utf-8"));
+try {
+  let { ip: oldIp } = JSON.parse(fs.readFileSync("./ip.json", "utf-8"));
   // import a from "./ip.json";
   console.log(
     "%c [ oldIp ]: ",
     "color: #bf2c9f; background: pink; font-size: 13px;",
     oldIp
   );
-  
+
   let newIp;
   http("http://ip-api.com/json")
     .then((res) => {
@@ -34,7 +60,6 @@ try{
     .catch((e) => {
       console.log(e);
     });
-}catch(e){
+} catch (e) {
   console.log(e);
 }
-
